bf07e2db01a2d4c6c0e4e91a42c20ba0ce9cd8b9
CodingMissDaisy <16388611+CodingMissDaisy@users.noreply.github.com>
2023-11-16 11:19:37 -0500

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,69 @@
+
+// Pseudocode in your own words the steps you would take to complete each of the component requirements below:
+
+
+
+// Component Requirements:
+    // Build a Login component that accepts the users email and password.
+    
+// Pseudocode:
+    // 
+    // 
+    // 
+
+
+
+
+
+
+
+// Component Requirements:
+    // Build a side menu component that has 5 buttons.
+
+// Pseudocode:
+    // 
+    // 
+    // 
+
+
+
+
+
+
+
+// Component Requirements:
+    // Build a filter component that can filter chart data.
+
+// Pseudocode:
+    // 
+    // 
+    // 
+
+
+
+
+
+
+
+// Component Requirements:
+    // Build a profile component where users can add and edit their profile information.
+
+// Pseudocode:
+    // 
+    // 
+    // 
+    // 
+
+
+
+
+
+
+// Component Requirements:
+    // Build a bar chart component that displays 2 sets of data from the backend.
+
+// Pseudocode:
+    // 
+    // 
+    // 
+    // 
---
+++ README.md
@@ -0,0 +1,3 @@
+Instructions:
+
+In the Assignment.js file, pseudocode in your own words the steps you would take to complete each of the component requirements.
