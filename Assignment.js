267316fb8176f8c1ea800b3ccc5ed262d9542665
tberry825 <tracief515@gmail.com>
2023-11-16 14:35:49 -0600

    sunmitting assignment

--- Assignment.js
+++ Assignment.js
@@ -7,41 +7,29 @@
     // Build a Login component that accepts the users email and password.
     
 // Pseudocode:
-    // 
-    // 
-    // 
+    // Create the component folder is its not already there.
+    // create the file for the component that will house the component
+    // create the Login function that gives the user the ability to input the email and password
 
-
-
-
-
-
-
+  
 // Component Requirements:
     // Build a side menu component that has 5 buttons.
 
 // Pseudocode:
-    // 
-    // 
-    // 
-
-
-
-
-
+    // side menu component file in components folder
+    // create button component file in components folder
+    // create side bar in App.js
+    // pass in buttons in the side bar in App 
 
 
 // Component Requirements:
     // Build a filter component that can filter chart data.
 
 // Pseudocode:
-    // 
-    // 
-    // 
-
-
-
-
+    // Create filter component
+    // create filter function that will filter the data 
+    // add filter to the chart
+    // create function that will remove data when
 
 
 
@@ -49,13 +37,12 @@
     // Build a profile component where users can add and edit their profile information.
 
 // Pseudocode:
-    // 
-    // 
-    // 
-    // 
-
-
-
+    // create a profile component and also a list / object for what information is needed for the user
+    // create page that will disply user info
+    // create an edit button component the will allow you to go bck to the oringinal user profile page and edit info
+    // make inputs required, and prompt if fields are not correctly inputted
+    // create save button to save information on profile page
+    // save button allows user to see new info on page and continue on website
 
 
 
@@ -63,7 +50,8 @@
     // Build a bar chart component that displays 2 sets of data from the backend.
 
 // Pseudocode:
-    // 
-    // 
-    // 
-    // 
+    // create a function that grabs the 1 set of data from the backend
+    // create a function that grabs the 2 set of data from the backend
+    // create a bar chart component that displays both sets of information 
+    // display the bar chart
+    // display "data currently unavailable" if no data is present
